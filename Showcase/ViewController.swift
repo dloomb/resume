3db70fc59db8dc5ed61a83ed134a9dc32a93acc6
Daniel Loomb <daniel@meamobile.com>
2016-07-25 16:42:35 -0700

    Initial Commit

---
+++ Showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Showcase
+//
+//  Created by Daniel Loomb on 7/25/16.
+//  Copyright © 2016 Daniel Loomb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
